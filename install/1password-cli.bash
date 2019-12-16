b461c8d254921c624bc64caff1d9be0de118874e
BooleanCat <thgodkin@gmail.com>
2019-12-16 11:32:05 +0000

    install 1password-cli

---
+++ install/1password-cli.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [ "$( op -v )" == '0.8.0' ]; then
+  echo 'already installed'
+  exit 0
+fi
+
+curl -L --silent https://cache.agilebits.com/dist/1P/op/pkg/v0.8.0/op_linux_amd64_v0.8.0.zip > download/op_linux_amd64_v0.8.0.zip
+
+unzip -o download/op_linux_amd64_v0.8.0.zip -d bin/
+
+rm download/op_linux_amd64_v0.8.0.zip
+rm bin/op.sig
+
