211da288f8c13e7d2d5f27351dbfc4df563645a5
BooleanCat <thgodkin@gmail.com>
2019-12-16 13:45:06 +0000

    add go

---
+++ install/golangci-lint.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [[ "$( golangci-lint --version )" =~ '1.21.0' ]]; then
+  echo 'already installed'
+  exit 0
+fi
+
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b /home/tom/workspace/machine-popos/bin v1.21.0
