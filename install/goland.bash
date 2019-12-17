079ef263efc267ebdc37ae181435b43c2361b4a6
BooleanCat <thgodkin@gmail.com>
2019-12-17 17:06:16 +0000

    skip goland if installed

--- install/goland.bash
+++ install/goland.bash
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-#if [[ "$( docker-compose -v )" =~ '1.25.0' ]]; then
-#  echo 'already installed'
-#  exit 0
-#fi
+if [ -d apps/GoLand-2019.3 ]; then
+  echo 'already installed'
+  exit 0
+fi
 
 curl -L --silent https://download.jetbrains.com/go/goland-2019.3.tar.gz -o download/goland-2019.3.tar.gz
 tar -xzf download/goland-2019.3.tar.gz -C apps/
