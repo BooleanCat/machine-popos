5fea55357c46dc1e098361eb179fee60afad03e6
BooleanCat <thgodkin@gmail.com>
2019-12-17 10:52:44 +0000

    fix docker-compose

---
+++ install/docker-compose.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [[ "$( docker-compose -v )" =~ '1.25.0' ]]; then
+  echo 'already installed'
+  exit 0
+fi
+
+curl -L --silent "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$( uname -s )-$( uname -m )" -o bin/docker-compose
+chmod +rx bin/docker-compose
