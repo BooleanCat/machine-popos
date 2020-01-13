de195ddb1fce38939e3f53df0f3793456e762e74
BooleanCat <thgodkin@gmail.com>
2020-01-13 13:47:53 +0000

    downgrade golangci-lint to a version that doesn't flake

--- install/golangci-lint.bash
+++ install/golangci-lint.bash
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [[ "$( golangci-lint --version )" =~ '1.22.2' ]]; then
+if [[ "$( golangci-lint --version )" =~ '1.19.1' ]]; then
   echo 'already installed'
   exit 0
 fi
 
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b /home/tom/workspace/machine-popos/apps/bin v1.22.2
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b /home/tom/workspace/machine-popos/apps/bin v1.19.1
